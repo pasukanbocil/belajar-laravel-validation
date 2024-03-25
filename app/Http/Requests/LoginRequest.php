3c983a42e6d36ae9b8fcabf31b51712b8ad1ae95
pasukanbocil <ctian8787@gmail.com>
2024-03-25 12:52:28 +0700

    Add LoginRequest and logging in FormController

---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'username' => ['required', 'email', 'max:100'],
+            'password' => ['required', Password::min(6)->letters()->numbers()->symbols()]
+        ];
+    }
+
+    protected function prepareForValidation(): void
+    {
+        $this->merge([
+            'username' => strtolower($this->input("username"))
+        ]);
+    }
+
+    protected function passedValidation(): void
+    {
+        $this->merge([
+            'password' => bcrypt($this->input("password"))
+        ]);
+    }
+}
